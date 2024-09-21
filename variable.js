58091ddd5de73d0a1070576493560aa990098117
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-09-21 20:18:17 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,15 @@
+let collge="kaveri college";
+var name="Kaveri";
+const Id=123;
+
+ let rollno;
+
+ subject="science";
+
+
+ collge=" college";
+ var name="gauree";
+
+//  Id=33;
+
+ console.table([collge,name,Id,rollno,subject]);
