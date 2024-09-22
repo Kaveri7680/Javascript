52b91047307f59d669378ee970b8128d7bec742e
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-09-22 15:14:09 +0530

    Add files via upload

---
+++ Datatype.js
@@ -0,0 +1,31 @@
+
+//Number
+let age=23;
+console.log("Number datatype:"+age);
+
+//String
+let name="kaveri";    //write String in ' ' or " " quotes
+console.log("String datatype:"+name);
+
+//boolean
+let isVisible=true;     // boolen has 2 value true or false
+console.log("Boolen datatype:"+isVisible);
+
+//undefined
+let data;
+console.log("undefined datatype:"+data);
+
+//null
+let mark=null;
+console.log("Null datatype:"+mark);
+
+//BigInt    
+let cost=BigInt(1234445);
+console.log("BigInt datatype:"+cost);
+
+//object   
+// object define in {} bracket with key:value pair
+const student={ fullName:"kaveri gaudase",age:23,cgpa:8.55};
+student.fullName="gauree gaudase";
+
+console.log(student);
